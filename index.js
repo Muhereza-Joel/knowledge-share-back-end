17fac23931131362a5d883e6842d959e776ff7d2
Muhereza Joel <muherezajoel40@gmail.com>
2023-12-09 15:17:25 +0300

    removed route handling from index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const routes = require('./routes/auth');
 
 const app = express();
 const port = 3001;
@@ -9,27 +10,8 @@ const port = 3001;
 app.use(bodyParser.json());
 app.use(cors());
 
-// Fake user data for authentication
-const fakeUserData = [
-  { email: 'test@example.com', password: 'password123' },
-  // Add more fake user data as needed
-];
-
-// Authentication endpoint
-app.post('/api/login', (req, res) => {
-  const { email, password } = req.body;
-
-  // Check if the provided credentials match any user
-  const user = fakeUserData.find((user) => user.email === email && user.password === password);
-
-  if (user) {
-    // Simulate a successful login
-    res.json({ success: true, message: 'Login successful' });
-  } else {
-    // Simulate a failed login
-    res.status(401).json({ success: false, message: 'Invalid credentials' });
-  }
-});
+// Use the routes
+app.use('/api/auth', routes);
 
 // Start the server
 app.listen(port, () => {
---
+++ routes/auth.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const router = express.Router();
+
+// Fake user data for authentication
+const fakeUserData = [
+    { email: 'test@example.com', password: 'password123' },
+  ];
+
+router.post("/login", (req, res) => {
+  const { email, password } = req.body;
+
+  // Check if the provided credentials match any user
+  const user = fakeUserData.find(
+    (user) => user.email === email && user.password === password
+  );
+
+  if (user) {
+    res.json({ success: true, message: "Login successful" });
+
+  } else {
+    res.status(401).json({ success: false, message: "Invalid credentials" });
+  }
+});
+
+router.post("/register", (req, res) => {
+  const { email, password } = req.body;
+  const userExists = fakeUserData.find((user) => user.email === email);
+
+  if (userExists) {
+    res
+      .status(400)
+      .json({ success: false, message: "Email is already registered" });
+  } else {
+
+    fakeUserData.push({ email, password });
+
+    res.json({ success: true, message: "Registration successful" });
+  }
+});
+
+module.exports = router;
