76d764b7a00a36e95e7a441ccd5c712d92c81384
Muhereza Joel <muherezajoel40@gmail.com>
2024-02-03 17:57:28 +0300

    added functionality for editing and deleting events

--- mysql/queries/eventQueris.js
+++ mysql/queries/eventQueris.js
@@ -5,7 +5,7 @@ const saveEvent = (title, startDate, endDate, userId, callback) => {
   const id = uuidv4();
 
   const query = `INSERT INTO events (id, title, start_date, end_date, user_id) 
-  VALUES (?, ?, ?, ?, ?);
+    VALUES (?, ?, ?, ?, ?);
   `;
 
   const values = [id, title, startDate, endDate, userId];
@@ -15,17 +15,59 @@ const saveEvent = (title, startDate, endDate, userId, callback) => {
       return callback(error, null);
     }
 
-    callback(null, null);
+    // Fetch the inserted data by querying the inserted ID
+    const selectQuery = 'SELECT * FROM events WHERE id = ?';
+    pool.query(selectQuery, [id], (selectError, selectResult) => {
+      if (selectError) {
+        return callback(selectError, null);
+      }
+
+      // Returning the data about the inserted row
+      callback(null, selectResult[0]);
+    });
   });
 };
 
+
+const editEvent = (eventId, title, startDate, endDate, callback) => {
+  const query = `UPDATE events SET title = ?, start_date = ?, end_date = ? WHERE id = ?`;
+
+  const values = [title, startDate, endDate, eventId];
+
+  pool.query(query, values, (error, result) => {
+    if(error) {
+      return callback(error, null);
+    }
+
+    callback(null, null)
+  })
+};
+
+const deleteEvent = (eventId, callback) => {
+  const query = `DELETE FROM events WHERE id = ?`;
+  
+  pool.query(query, [eventId], (error, result) => {
+    if(error) {
+      return callback(error, null)
+    }
+
+    callback(null, null)
+  })
+}
+
 const getAllEvents = (userId, callback) => {
-    pool.query("SELECT * FROM events WHERE user_id = ?", [userId], (error, results) => {
+  pool.query(
+    "SELECT * FROM events WHERE user_id = ?",
+    [userId],
+    (error, results) => {
       callback(error, results);
-    });
-  };
+    }
+  );
+};
 
 module.exports = {
   saveEvent,
-  getAllEvents
+  getAllEvents,
+  editEvent,
+  deleteEvent
 };
--- routes/event.js
+++ routes/event.js
@@ -3,27 +3,26 @@ const router = express.Router();
 const eventQueries = require("../mysql/queries/eventQueris")
 
 router.post("/add", (req, res) => {
-    try {
-        const {title, start, end, userId} = req.body;
-    
-       
-        eventQueries.saveEvent(title, start, end, userId, (error, result) => {
-            if (error) {
-              console.error("Error saving question information:", error);
-              res.status(500).json({ message: "Error saving question information" });
-            } else {
-              res
-                .status(200)
-                .json({ message: "Event submitted successfully!" });
-            }
-          }
-        );
-      } catch (error) {
-        console.error("Error processing the request:", error);
-        res.status(500).json({ message: "Internal Server Error" });
-      }
+  try {
+    const { title, start, end, userId } = req.body;
 
-})
+    eventQueries.saveEvent(title, start, end, userId, (error, eventData) => {
+      if (error) {
+        console.error("Error saving event information:", error);
+        res.status(500).json({ message: "Error saving event information" });
+      } else {
+        res.status(200).json({
+          message: "Event submitted successfully!",
+          eventData: eventData, // Include the inserted event data in the response
+        });
+      }
+    });
+  } catch (error) {
+    console.error("Error processing the request:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
 
 router.post("/all", (req, res) => {
     try {
@@ -38,6 +37,41 @@ router.post("/all", (req, res) => {
       res.status(500).json({ error: "Internal Server Error" });
     }
   });
+
+  router.put("/update/:eventId", (req, res) => {
+    try {
+      const eventId = req.params.eventId;
+      const {title, start, end} = req.body;
+
+      eventQueries.editEvent(eventId, title, start, end, (error, results) => {
+        if(error){
+          throw error
+        }
+
+        res.json({message : "Event Updated Successfully"})
+      })
+
+    } catch (error) {
+      res.status(500).json({error: "Internal Server Error" })
+    }
+  })
+
+  router.delete("/delete/:eventId", (req, res) => {
+    try {
+      const eventId = req.params.eventId;
+
+      eventQueries.deleteEvent(eventId, (error, results) => {
+        if(error){
+          throw error
+        }
+
+        res.json({message : "Event Deleted Successfully"})
+      })
+      
+    } catch (error) {
+      res.status(500).json({error: "Internal Server Error" })
+    }
+  })
   
 
 module.exports = router;
