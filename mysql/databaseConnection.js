02432a9736f7aaa339f1d202830b04d37747441f
Muhereza Joel <muherezajoel40@gmail.com>
2024-02-07 13:02:23 +0300

    changed database to hosted version

--- mysql/databaseConnection.js
+++ mysql/databaseConnection.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql");
 
 const pool = mysql.createPool({
-  host: "localhost",
+  host: "ks-db.chg2u22ew259.eu-north-1.rds.amazonaws.com",
   user: "root",
-  password: "",
-  database: "knowledge_share_db",
+  password: "12345678",
+  database: "ks-db",
 });
 
 module.exports = pool;
